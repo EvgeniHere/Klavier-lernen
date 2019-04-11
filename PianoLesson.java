a8ff78b6658c752841e317e88674e145383def3d
EvgeniUschakov <evgeni.uschakov@gmail.com>
2019-04-12 00:35:15 +0200

    Update PianoLesson.java

--- PianoLesson.java
+++ PianoLesson.java
@@ -9,10 +9,6 @@ public class PianoLesson {
     static String path;
     
     public static void main(String[] args) {
-        File currDir = new File(".");
-        path = currDir.getAbsolutePath();
-        path = path.substring(0, path.length()-1);
-        
         Quintenzirkel qz = new Quintenzirkel();
         Keys_Holder keys_Holder = new Keys_Holder();
         Tonleiter_Holder tonleiter_Holder = new Tonleiter_Holder();
